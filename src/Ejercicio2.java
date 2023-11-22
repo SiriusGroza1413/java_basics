f4ee5c8bb9c447d05bb1b2f1476b55f45adb19aa
Guillermo Ojeda <gro1413@hotmail.com>
2023-11-22 14:30:16 -0300

    Ejercicio2 done

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,3 +1,16 @@
 public class Ejercicio2 {
     
+    public static void main(String[] args) {
+        
+        int val1 = 20;
+        int val2 = 10;
+
+        if(val1 > val2){
+            System.out.println((val1) + " es mayor que " + (val2));
+        } else if (val1 < val2) {
+            System.out.println((val1) + " es menor que " + (val2));
+        } else {
+            System.out.println("Ambos valores son iguales");
+        }
+    }
 }
