7d2db2151ef5503317193ef10dd2ee45edbde9db
Guillermo Ojeda <gro1413@hotmail.com>
2023-11-22 13:42:43 -0300

    add Ejercicio1

--- src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        int val1 = 5;
-        int val2 = 15;
-    }
-}
---
+++ src/Ejercicio1.java
@@ -0,0 +1,14 @@
+public class Ejercicio1 {
+    
+    public static void main(String[] args) {
+
+        int val1 = 10;
+        int val2 = 5;
+
+        System.out.println("La suma de los numeros es: " + (val1 + val2));
+        System.out.println("La resta de los numeros es: " + (val1 - val2));
+        System.out.println("La multiplicacion de los numeros es: " + (val1 * val2));
+        System.out.println("La division de los numeros es: " + (val1 / val2));
+        System.out.println("El resto de los numeros es: " + (val1 % val2));
+    }
+}
