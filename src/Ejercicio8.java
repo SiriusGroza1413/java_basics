0336c661a3c2052315fd5449cc864b7bc1b79f9c
Guillermo Ojeda <gro1413@hotmail.com>
2023-11-24 12:57:02 -0300

    Ejercicio8 done

--- src/Ejercicio8.java
+++ src/Ejercicio8.java
@@ -1,3 +1,13 @@
+import javax.swing.JOptionPane;
+
 public class Ejercicio8 {
-    
+
+    public static void main(String[] args) {
+        
+        int totalHours = Integer.parseInt(JOptionPane.showInputDialog("Introduce horas totales de este mes"));
+
+        int totalPay = totalHours * 15;
+
+        JOptionPane.showMessageDialog(null, "El sueldo de este mes segun tus horas corresponde a " + "$" + totalPay);
+    }
 }
