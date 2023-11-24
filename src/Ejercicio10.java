f156cd2349dd478daddfc8740af8d8c8a1dafe88
Guillermo Ojeda <gro1413@hotmail.com>
2023-11-24 15:13:11 -0300

    Ejercicio10 done

--- src/Ejercicio10.java
+++ src/Ejercicio10.java
@@ -2,5 +2,15 @@ public class Ejercicio10 {
     
     public static void main(String[] args) {
         
+        for (int i = 1; i < 101; i++) {
+            System.out.println(i);
+        }
+
+        int n = 1;
+
+        while (n < 101) {
+            System.out.println(n);
+            n++;
+        }
     }
 }
