434f2fd9fac56eb84ef05404a8d5b38d0c019022
Guillermo Ojeda <gro1413@hotmail.com>
2023-11-24 14:59:20 -0300

    Ejercicio9  done

---
+++ src/Ejercicio9.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio9 {
+
+    public static void main(String[] args) {
+        Scanner tool = new Scanner(System.in);
+
+        double iva = 0.21;
+
+        System.out.print("Ingrese el precio del producto: ");
+        double price = tool.nextDouble();
+
+        double totalPrice = price + (price * iva);
+
+        System.out.println("El precio total de pruducto + IVA 21% es: " + totalPrice);
+
+    }
+}
