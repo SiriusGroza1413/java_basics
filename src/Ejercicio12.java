1e63dcc040c97b02a3df89e9a9025231c1f806e0
Guillermo Ojeda <gro1413@hotmail.com>
2023-11-24 19:16:07 -0300

    Ejercicio12 done

--- src/Ejercicio12.java
+++ src/Ejercicio12.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Ejercicio12 {
 
     public static void main(String[] args) {
         
+        Scanner tool = new Scanner(System.in);
+
+        System.out.println("Introduce el numero de ventas totales");
+        int selled = tool.nextInt();
+
+        double sum = 0;
+
+        
+        for (int i = 1; i <= selled; i++) {
+            System.out.println("Ingrese el valor de la venta " + i + ":");
+            double totalAmount = tool.nextDouble();
+            sum = sum + totalAmount;
+        } 
+
+        System.out.println("La suma total de todas las ventas es de: $" + sum);
+
+        tool.close();
+
     }
     
 }
