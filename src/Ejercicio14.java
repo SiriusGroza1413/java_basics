06bade553e0993610bee39ad73b3f4f96bf28118
Guillermo Ojeda <98858387+SiriusGroza1413@users.noreply.github.com>
2023-11-27 19:35:14 -0300

    Rechange variable name

--- src/Ejercicio14.java
+++ src/Ejercicio14.java
@@ -5,7 +5,7 @@ public class Ejercicio14 {
     public static void main(String[] args){
 
         // declarar la contraseña
-        String password = "bai";
+        String password = "iLikeMyGymsAss";
         int counter = 0;
         String userPass = "";
         boolean isCorrect = false;
