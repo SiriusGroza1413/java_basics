ab1c71a421851ede94034fe70aa3b085e0771066
Guillermo Ojeda <gro1413@hotmail.com>
2023-11-27 19:22:16 -0300

    add Ejercicio14 and  done

---
+++ src/Ejercicio14.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio14 {
+    
+    public static void main(String[] args){
+
+        // declarar la contraseña
+        String password = "iLikeMyGymsAss";
+        int counter = 0;
+        String userPass = "";
+        boolean isCorrect = false;
+
+      
+            while (isCorrect == false && counter < 3) { //mientras el numero de intentos sea menor a 3 y no sea correcta...
+
+                userPass = JOptionPane.showInputDialog("Introduce la contraseña");
+
+                if (!password.equals(userPass)) {                                                           //comparacion
+                    JOptionPane.showMessageDialog(null, "Contraseña incorrecta!");
+                    counter++;                                                                   //aumento el numero de intentos 
+                } else {
+                    isCorrect = true;                                                   //seteo el validador a true para salr del bucle
+                    JOptionPane.showMessageDialog(null, "Contraseña correcta");
+                }
+            }
+
+    
+    }
+}
