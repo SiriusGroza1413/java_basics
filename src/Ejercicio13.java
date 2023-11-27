8be5603f4bac5a5a3ea81b25ade89f573d1384b3
Guillermo Ojeda <gro1413@hotmail.com>
2023-11-27 18:38:28 -0300

    Ejercicio13 done

--- src/Ejercicio13.java
+++ src/Ejercicio13.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Ejercicio13 {
     
     public static void main(String[] args) {
-        
+
+        Scanner tool = new Scanner(System.in);
+
+        int num = 0;
+
+        do {
+            System.out.println("Introduce un numero menor a cero");
+            num = tool.nextInt();
+        } while (num < 0);
+
+        System.out.println("Numero: " + num);
+
+        tool.close();
     }
+
 }
