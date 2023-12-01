8acec35649338fcef5e9427edb2df61bcf1c61af
Guillermo Ojeda <gro1413@hotmail.com>
2023-12-01 20:07:26 -0300

    Ejercicio19 done

--- src/Ejercicio19.java
+++ src/Ejercicio19.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Ejercicio19 {
     
     public static void main(String args[]){
 
+        int factorial = 1;
+        int inputNum = 0;
+
+        Scanner tool = new Scanner(System.in);
+
+        System.out.println("Introduce un numero para calcular su factorial");
+        inputNum = tool.nextInt();
+
+        tool.close();
+        
+        for (int i = 1; i <= inputNum; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("El factorial de: " + inputNum + " es " + factorial );
     }
-    
 }
